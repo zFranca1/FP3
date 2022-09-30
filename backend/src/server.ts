320379d04c357c216326827dab220ab235e3ef1f
zFranca1 <joaopedro.franca1@hotmail.com>
2022-09-30 11:53:38 -0300

    Criando usuarios com rota

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,28 @@
 import express from "express";
-import './database/connection';
+import { getRepository } from "typeorm";
 
-const PORT = process.env.PORT || 3333;
+import "./database/connection";
+import User from "./models/User";
 
 const app = express();
 
-app.listen(PORT as number, () => console.log(`listening on all interfaces:${PORT}`));
+app.use(express.json());
 
+app.post("/users", async (req, res) => {
+  console.log(req.body);
+  
+  const { nome, salario } = req.body;
 
+  const userRepository = getRepository(User);
+
+  const user = userRepository.create({
+    nome,
+    salario,
+  });
+
+  await userRepository.save(user);
+
+  return res.status(201).json(user);
+});
+
+app.listen(3333);
