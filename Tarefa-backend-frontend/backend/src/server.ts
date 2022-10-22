2bcd8c55a9b7361b55205fe156daa0f408d63b78
franca1-Arch <joaopedro.franca1@hotmail.com>
2022-10-22 17:25:58 -0300

    quase deu ruim

--- Tarefa-backend-frontend/backend/src/server.ts
+++ Tarefa-backend-frontend/backend/src/server.ts
@@ -1,48 +1,14 @@
 import express from "express";
-<<<<<<< HEAD
-import { getRepository } from "typeorm";
-
-import "./database/connection";
-import User from "./models/User";
-
-const app = express();
-
-app.use(express.json());
-
-app.post("/users", async (req, res) => {
-  console.log(req.body);
-  
-  const { nome, salario } = req.body;
-
-  const userRepository = getRepository(User);
-
-  const user = userRepository.create({
-    nome,
-    salario,
-  });
-
-  await userRepository.save(user);
-
-  return res.status(201).json(user);
-});
-
-app.listen(3333);
-=======
 import { AppDataSource } from "./data-source";
-var cors = require('cors')
+var cors = require('cors');
 import routes from "./routes";
 
 const PORT = process.env.PORT || 3333;
 
 const app = express();
 
-var corsOptions = {
-  origin: 'http://localhost:8100',
-  optionsSuccessStatus: 200 
-}
 app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 AppDataSource.initialize()
@@ -52,6 +18,3 @@ AppDataSource.initialize()
     );
   })
   .catch((error) => console.log(error));
-
-
->>>>>>> 5c0f297 (a)
